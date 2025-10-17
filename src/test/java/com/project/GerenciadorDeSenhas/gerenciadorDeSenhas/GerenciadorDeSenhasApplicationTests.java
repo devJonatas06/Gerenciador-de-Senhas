2450b144e840c5fe4f4da84dec63998b6c94497f
JonataFreitas <jonatasaple@gmail.com>
2025-10-17 19:20:07 -0300

    add repository

---
+++ src/test/java/com/project/GerenciadorDeSenhas/gerenciadorDeSenhas/GerenciadorDeSenhasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.GerenciadorDeSenhas.gerenciadorDeSenhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeSenhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
