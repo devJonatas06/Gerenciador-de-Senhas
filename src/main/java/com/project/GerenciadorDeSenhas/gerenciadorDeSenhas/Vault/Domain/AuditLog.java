71d0c376cbfbc49cb126097898ecc36718b95476
JonataFreitas <jonatasaple@gmail.com>
2025-11-19 10:11:25 -0300

    Add AuditLog, Para melhorar o controle dos log, criando um historico e facilitando pra achar erros

---
+++ src/main/java/com/project/GerenciadorDeSenhas/gerenciadorDeSenhas/Vault/Domain/AuditLog.java
@@ -0,0 +1,19 @@
+package com.project.GerenciadorDeSenhas.gerenciadorDeSenhas.Vault.Domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "audit_logs")
+@Getter
+@Setter
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userEmail;
+    private String action;
+    private LocalDateTime timestamp = LocalDateTime.now();
+}
