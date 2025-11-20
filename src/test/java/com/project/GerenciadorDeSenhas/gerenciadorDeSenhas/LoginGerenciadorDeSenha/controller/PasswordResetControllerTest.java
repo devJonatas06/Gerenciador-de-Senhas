779d84617e3ae55a47121a57ddd58fda3a1ce3dc
JonataFreitas <jonatasaple@gmail.com>
2025-11-20 19:11:14 -0300

    feat add AuthControllerTest

---
+++ src/test/java/com/project/GerenciadorDeSenhas/gerenciadorDeSenhas/LoginGerenciadorDeSenha/controller/PasswordResetControllerTest.java
@@ -0,0 +1,7 @@
+package com.project.GerenciadorDeSenhas.gerenciadorDeSenhas.LoginGerenciadorDeSenha.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordResetControllerTest {
+
+}
